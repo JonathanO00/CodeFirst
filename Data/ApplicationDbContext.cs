3f6bbfd253539bc8aaed51c82d45ee8a30ca9f5a
Jonathan Orfani <jonathanorfani@gmail.com>
2021-10-07 10:48:24 -0700

    Added script

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CodeFirst.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,19 @@ namespace CodeFirst.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Team>().HasData(SampleData.GetTeams());
+            modelBuilder.Entity<Player>().HasData(SampleData.GetPlayers());
         }
     }
 }
